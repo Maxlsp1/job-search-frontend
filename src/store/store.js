50a82db268103650af9e6e8fa304a47ae7b57e15
Maxime Rault <m.rault.sen@gmail.com>
2022-06-27 23:34:56 +0200

    work on store

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from "./reducers/user"
+
+const store = configureStore({
+
+  reducer: {
+    user: userReducer
+  }
+
+})
+
+export default store;
